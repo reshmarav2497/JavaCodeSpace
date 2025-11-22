9f102099886e72017e017c77984daf852307ca24
reshma <reshmaraveendra2497@gmail.com>
2025-11-22 21:35:42 +0530

    OOps - 6

--- OOPS/src/com/oops/chapter6/Main.java
+++ OOPS/src/com/oops/chapter6/Main.java
@@ -2,6 +2,16 @@ package com.oops.chapter6;
 
 public class Main {
     public static void main(String[] args) {
+        List items = new List();
+
+        items.add(1);
+        System.out.println(items.get(0));
+
+        GenericList<Integer> items1 = new GenericList<>();
+        items1.add(1);
+        items1.add(2);
+
+        System.out.println(items1.get(1));
 
     }
 }
