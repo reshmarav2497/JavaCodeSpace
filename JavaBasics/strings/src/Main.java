ca26f5f181557272c0383cfaa85f0564914a1ab1
reshma <reshmaraveendra2497@gmail.com>
2025-12-26 02:16:22 +0530

    strings - 1

--- JavaBasics/strings/src/Main.java
+++ JavaBasics/strings/src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
 
+        //1) String Literal(String Pool)
+        String s1 = "Java";
+        //String s2 = "Java";
+
+        //2) using new Keyword(Heap memory)
+        String s2 = new String("Java");
+
+        System.out.println("s2 => " + s2);
+
+        //Immutability
+        s2 = "OOPS";
+
+        System.out.println("s2 => " + s2);
+
+        /*
+        => Comparison
+        1) == -> reference check
+        2) equals() -> content check
+         */
+        System.out.println("s1 == s2 => " + (s1 == s2));
+
+        System.out.println("s1.equals(s2) => " + s1.equals(s2));
+
     }
 }
