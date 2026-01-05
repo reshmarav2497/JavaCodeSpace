5aa553431f059e0df5a73c3306ca9ba5de7f4d94
reshma <reshmaraveendra2497@gmail.com>
2026-01-06 01:48:08 +0530

    patterns - 4

--- JavaBasics/patterns/src/Main.java
+++ JavaBasics/patterns/src/Main.java
@@ -43,6 +43,37 @@ public class Main {
          */
         Patterns.pattern6(n);
 
+        System.out.println("--------------------------");
+        /*
+                1
+               212
+              32123
+             4321234
+            543212345
+         */
+        Patterns.pattern7(n);
+
+        System.out.println("--------------------------");
+        /*
+                1
+               212
+              32123
+               212
+                1
+         */
+        Patterns.pattern8(n);
+
+        System.out.println("--------------------------");
+        /*
+               3 3 3 3 3
+               3 2 2 2 3
+               3 2 1 2 3
+               3 2 2 2 3
+               3 3 3 3 3
+         */
+        Patterns.pattern9(n);
+
+
         sc.close();
     }
 }
--- JavaBasics/patterns/src/Patterns.java
+++ JavaBasics/patterns/src/Patterns.java
@@ -71,4 +71,76 @@ public class Patterns {
             System.out.println();
         }
     }
+
+    public static void pattern7(int n){
+        for (int i = 1; i <= n; i++) {
+
+            //spaces
+            for (int s = 1; s <= (n - i); s++) {
+                System.out.print(" ");
+            }
+
+            //columns
+
+            for(int j = i; j >= 1; j--){
+                System.out.print(j);
+            }
+
+            for(int k = 2; k <= i; k++){
+                System.out.print(k);
+            }
+
+
+            System.out.println();
+        }
+
+    }
+
+    public static void pattern8(int n) {
+        for (int i = 1; i <= 2 * n - 1; i++) {
+
+            //spaces
+            int spaces = (i <= n) ? (n - i) : (i - n);
+            for (int s = 1; s <= spaces; s++) {
+                System.out.print(" ");
+            }
+
+            //columns
+
+            int cols = (i <= n) ? i : n - (i - n);
+            for (int j = cols; j >= 1; j--) {
+                System.out.print(j);
+            }
+
+            for (int k = 2; k <= cols; k++) {
+                System.out.print(k);
+            }
+
+
+            System.out.println();
+        }
+    }
+
+    public static void pattern9(int n) {
+        int size = 2 * n - 1;
+
+        for (int i = 1; i <= size; i++) {
+
+            for (int j = 1; j <= size; j++) {
+
+                //calculate distance from each side
+
+                int top = i - 1;
+                int left = j - 1;
+                int bottom = size - i;
+                int right = size - j;
+
+                int minDist = Math.min(Math.min(top, bottom), Math.min(left, right));
+
+                System.out.print((n - minDist) + " ");
+            }
+
+            System.out.println();
+        }
+    }
 }
